510428f0b2df6969ef196467c20571ea7d8d197b
Felipe <felipe.macedo00@gmail.com>
2026-06-13 09:49:13 -0300

    chore(api): conexão com com o mysql atrvés do sequelize

--- api/server.ts
+++ api/server.ts
@@ -1,8 +1,12 @@
 import app from "./src/app.js";
+import { connectMySQL } from "./src/config/database.js";
 
 const PORT = process.env.PORT || 3000;
 
-/* INICIALIZAÇÃO DO SERVIDOR */
+// Conecta ao banco de dados MySql
+await connectMySQL();
+
+// Após a conexão com o banco de dados, inicializa-se o servidor
 app.listen(PORT, () => {
     console.log(`Server rodando pela porta: http://localhost:${PORT}`);
 });
---
+++ api/src/config/database.ts
@@ -0,0 +1,32 @@
+import { Sequelize } from "sequelize";
+
+// Variáveis para a conexação do banco MySQL
+const dbName = process.env.DB_NAME as string;
+const dbUser = process.env.DB_USER as string;
+const dbHost = process.env.DB_HOST;
+const dbPassword = process.env.DB_PASSWORD;
+const dbPort = parseInt(process.env.DB_MYSQL_PORT || "3306", 10);
+
+/* CONEXÃO MYSQL ATRAVÉS DA INSTÃNCIA DO SEQUELIZE */
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    port: dbPort,
+    dialect: "mysql",
+    logging: false, // Desativa os logs de SQL no terminal
+    define: {
+        timestamps: true, // Cria automaticamente as colunas 'createdAt' e 'updatedAt' nas tabelas
+        underscored: true, // Transforma camelCase em snake_case no banco (ex: userId vira user_id)
+    }
+});
+
+export const connectMySQL = async (): Promise<void> => {
+    try {
+        await sequelize.authenticate();
+        console.log('MySQL conectado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao conectar ao MySQL:', error);
+        process.exit(1); // Fecha a aplicação caso o banco principal falhe
+    }
+};
+
+export default sequelize;
