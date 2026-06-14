d73fa7461bbdd0961aa6d07accbc428acefffc05
Felipe <felipe.macedo00@gmail.com>
2026-06-14 09:29:00 -0300

    fix(api): adicionar middleware cors para liberar acessos da porta do Angular

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,10 +2,18 @@ import express from "express";
 import bodyParser from 'body-parser';
 import routes from "./routes/index.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import cors from 'cors';
 
 const app = express();
 
 /* MIDDLEWARES GLOBAIS */
+
+// Ative o CORS especificamente para a porta do Angular
+app.use(cors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Intercepta as requisições recebidas e verifica se o corpo (Body) está no formato JSON
 app.use(bodyParser.json());
